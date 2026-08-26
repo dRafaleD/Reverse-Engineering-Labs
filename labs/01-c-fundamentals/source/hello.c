62cd805dc07183a2a0993fbae9feef5b22f393f5
Eren Erdoğan <erdoganeren48@gmail.com>
2026-08-26 15:44:32 +0300

    Add Day 1 C hello world exercise

---
+++ labs/01-c-fundamentals/source/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>    // Preprocessor directive to include external library files
+                      // Standard Input Output Header file for printf and scanf functions
+
+int main() {    // The main entry point function where program execution begins
+
+    printf("This is my first c code\n"); // Prints output data or text to the screen
+    return 0;   // Ends the main function and tells the OS that the program ran successfully
+}
