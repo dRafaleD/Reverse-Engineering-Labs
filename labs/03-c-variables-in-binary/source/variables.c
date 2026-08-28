1b9708e59118ad97cd0991d754520f5a122d941c
Eren Erdoğan <erdoganeren48@gmail.com>
2026-08-28 08:25:18 +0300

    Add Day 3 variables source

---
+++ labs/03-c-variables-in-binary/source/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char mychar = 'C';
+    int myinteger = 16;
+    float myfloat = 5.10f;
+    double mydouble = 213.568;
+
+    printf("%c\n", mychar);
+    printf("%d\n", myinteger);
+    printf("%f\n", myfloat);
+    printf("%f\n", mydouble);
+
+    return 0;
+}
