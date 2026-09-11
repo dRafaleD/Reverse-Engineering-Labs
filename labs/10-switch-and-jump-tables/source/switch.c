42ec539949d8f60f554436bd7d5d335360a186b6
Eren Erdoğan <erdoganeren48@gmail.com>
2026-09-11 06:08:38 -0700

    Add Day 10 switch statement source

---
+++ labs/10-switch-and-jump-tables/source/switch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int choice = 4;
+
+    switch (choice) {
+        case 1:
+            printf("Option 1\n");
+            break;
+        case 2:
+            printf("Option 2\n");
+            break;
+        case 3:
+            printf("Option 3\n");
+            break;
+        case 4:
+            printf("Option 4\n");
+            break;
+        case 5:
+            printf("Option 5\n");
+            break;
+        case 6:
+            printf("Option 6\n");
+            break;
+        default:
+            printf("Unknown option\n");
+            break;
+    }
+
+    return 0;
+}
