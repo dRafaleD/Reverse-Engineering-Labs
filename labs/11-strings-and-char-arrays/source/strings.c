4c0e783883559cd4e2eb7e220172cc08141749d7
Eren Erdoğan <erdoganeren48@gmail.com>
2026-09-12 14:15:26 -0700

    Add Day 11 string and char array source

---
+++ labs/11-strings-and-char-arrays/source/strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[] = "Rafale";
+    char code[] = "RE_LAB_11";
+
+    printf("Name: %s\n", name);
+    printf("Code: %s\n", code);
+    printf("Name length: %zu\n", strlen(name));
+
+    return 0;
+}
